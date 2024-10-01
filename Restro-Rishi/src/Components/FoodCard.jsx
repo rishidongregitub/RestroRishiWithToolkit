3e584f6eaaa317f1ca4433b41b3d1aafc4fb9152
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-10-01 03:27:29 +0000

    card rendring and some ui fixes

---
+++ Restro-Rishi/src/Components/FoodCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {AiFillStar} from 'react-icons/ai'
+
+const FoodCart = ({food}) => {
+    return (
+        <div className='font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2'>
+            <img src={food.img} alt={food.name} className='w-auto -[130px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out' />
+            <div className="text-sm flex justify-between">
+                <h2>{food.name}</h2>
+                <span className='text-green-500'>₹{food.price}</span>
+            </div>
+            <p className="text-sm font-normal">{food.desc.slice(0,50)}...</p>
+            <div className="flex justify-between">
+                <span className="flex justify-center items-center">
+                    <AiFillStar className='mr-1 text-yellow-400'/>{food.rating}
+                </span>
+                <button className='p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm'>Add to cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default FoodCart
--- Restro-Rishi/src/Components/FoodItems.jsx
+++ Restro-Rishi/src/Components/FoodItems.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import FoodCart from './FoodCart'
-
+import FoodCard from './FoodCard.jsx'
+import FoodData from '../Data/FoodData.js'
 const FoodItems = () => {
   return (
-    <div>
-      <FoodCart/>
+    <div className='flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10'>
+      {FoodData.map((food)=>{
+        return <FoodCard key={food.id} food={food}/>
+      })}
     </div>
   )
 }
