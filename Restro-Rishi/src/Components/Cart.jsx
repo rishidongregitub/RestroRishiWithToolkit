606ccc7d2df22e4e35ac3cc57a4e01e9a3ae2a18
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-10-03 17:31:36 +0000

    TotalCount with price

--- Restro-Rishi/src/Components/Cart.jsx
+++ Restro-Rishi/src/Components/Cart.jsx
@@ -8,7 +8,8 @@ import { useState } from 'react'
 const Cart = () => {
   const [activeCart, setActiveCart] = useState(true)
   const cartItems = useSelector((state) => state.cart.cart)
-  console.log(cartItems)
+  const totalQnty = cartItems.reduce((totalQnty, item)=> totalQnty + item.qnty, 0);
+  const totalPrice = cartItems.reduce((totalPrice, item)=> totalPrice + item.qnty * item.food.price, 0);
 
   return (
     <>
@@ -33,13 +34,13 @@ const Cart = () => {
           )
         }) : <h2 className='text-center text-xl font-bold text-gray-800'>Your cart is empty</h2> }
         <div className="absolute bottom-0">
-          <h3 className='font-semibold text-gray-800 '>Items :</h3>
-          <h3 className='font-semibold text-gray-800 '>Total Amount  :</h3>
+          <h3 className='font-semibold text-gray-800 '>Items : {totalQnty}</h3>
+          <h3 className='font-semibold text-gray-800 '>Total Amount : {totalPrice}</h3>
           <hr className='w-[90vw] lg:w-[18vw] py-2' />
           <button className='bg-green-500 font-bold px-3 text-white py-2 rounded-lg w-[90vw] lg:w-[18vw] mb-5'>Checkout</button>
         </div>
       </div>
-      <FaShoppingCart onClick={() => setActiveCart(!activeCart)} className='rounded-full bg-white shadow-md text-5xl p-3 fixed bottom-4 right-4' />
+      <FaShoppingCart onClick={() => setActiveCart(!activeCart)} className={`rounded-full bg-white shadow-md text-5xl p-3 fixed bottom-4 right-4 ${totalQnty > 0 && "animate-bounce delay-500 transition-all"}  `}/>
     </>
   )
 }
